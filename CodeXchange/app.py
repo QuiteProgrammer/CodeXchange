91e900d02d000b847baec35bc0d11666736caec3
sr7711 <115349704+sr7711@users.noreply.github.com>
2024-07-10 22:03:53 +0530

    Add files via upload

---
+++ CodeXchange/app.py
@@ -0,0 +1,11 @@
+# Define two numbers
+num1 = 5
+num2 = 3
+
+# Perform addition
+sum_result = num1 + num2
+
+# Now you can print the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+
