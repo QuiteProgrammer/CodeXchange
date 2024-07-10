87227f2c2437d111d2a079e58f000832d6a0cfb4
QuiteProgrammer <rayyanbaig.personal@gmail.com>
2024-07-10 22:06:08 +0530

    Comments

--- CodeXchange/app.py
+++ CodeXchange/app.py
@@ -1,11 +1,9 @@
-# Define two numbers
+#TEST CODE
 num1 = 5
 num2 = 3
 
-# Perform addition
 sum_result = num1 + num2
 
-# Now you can print the result
 print("The sum of", num1, "and", num2, "is:", sum_result)
 
 
